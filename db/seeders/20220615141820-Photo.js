5d487d41a08a0f876ad58160018f33e4107250cb
Pavel <gomer_74@mail.ru>
2022-06-16 11:16:16 +0300

    fix db

--- db/seeders/20220615141820-Photo.js
+++ db/seeders/20220615141820-Photo.js
@@ -12,7 +12,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://funart.pro/uploads/posts/2021-07/1626455212_5-funart-pro-p-detenish-vidri-zhivotnie-krasivo-foto-9.jpg',
       title: 'лыбится',
-      album_id: '2',
+      album_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -20,7 +20,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://funart.pro/uploads/posts/2021-07/1625531780_5-funart-pro-p-vidra-zverek-zhivotnie-krasivo-foto-5.jpg',
       title: 'кавказкая речная',
-      album_id: '2',
+      album_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -28,7 +28,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://web-zoopark.ru/wp-content/uploads/2018/07/4-126.jpg',
       title: 'балдеет',
-      album_id: '2',
+      album_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -36,7 +36,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://img.devrant.com/devrant/rant/r_2296425_Mt5EJ.jpg',
       title: 'мемы для интеллектуалов',
-      album_id: '3',
+      album_id: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -44,7 +44,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://i.pinimg.com/originals/c6/e3/b5/c6e3b567eb1ee88a9bfed10bf10bf9a8.jpg',
       title: 'это правда',
-      album_id: '3',
+      album_id: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -52,7 +52,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://intersog.co.il/handling/assets/uploads/2019/08/3-1-e1565688689401.png',
       title: 'красивая фотка',
-      album_id: '3',
+      album_id: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -60,7 +60,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://ak.picdn.net/shutterstock/videos/33426313/thumb/1.jpg',
       title: 'как же хочется нажать',
-      album_id: '4',
+      album_id: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -68,7 +68,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://www.formget.com/wp-content/uploads/2014/07/submit-form.jpg',
       title: 'enter?',
-      album_id: '4',
+      album_id: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -76,7 +76,7 @@ module.exports = {
     await queryInterface.bulkInsert('Photos', [{
       url: 'https://icon-library.com/images/submit-online-icon/submit-online-icon-20.jpg',
       title: '...',
-      album_id: '4',
+      album_id: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
